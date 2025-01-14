246087c35e52a2e7b3ba61b7e96c11ed7694051b
Pedro Ribeiro <47680931+tubarao312@users.noreply.github.com>
2025-01-14 18:41:39 +0000

    feat: Add durable and auto_delete config to BrokerConfig

--- client_sdks/python/src/broker/config.py
+++ client_sdks/python/src/broker/config.py
@@ -1,8 +1,14 @@
-from dataclasses import dataclass
+from pydantic import BaseModel
 
 
-@dataclass
-class BrokerConfig:
+class BrokerConfig(BaseModel):
     """Configuration for a broker."""
 
     url: str
+    """ The URL of the broker. """
+
+    durable: bool = True
+    """ Whether exchanges and queues are saved on disk."""
+
+    auto_delete: bool = False
+    """ Whether exchanges and queues are deleted when they are no longer in use."""
