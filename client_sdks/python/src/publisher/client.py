4f1e3cbfb3976c1b305729b881099fcde6d42fc7
Pedro Ribeiro <47680931+tubarao312@users.noreply.github.com>
2025-01-14 16:30:16 +0000

    refactor: Rework publisher client

--- client_sdks/python/src/publisher/client.py
+++ client_sdks/python/src/publisher/client.py
@@ -1,61 +1,79 @@
-import asyncio
-from uuid import UUID
+from typing import Optional
 
-from dataclasses import dataclass
+from uuid import UUID, uuid4
+from aiohttp_retry import RetryOptionsBase
 
+from broker import BrokerClient, BrokerConfig
 from manager import ManagerClient, ManagerConfig
-from models.task import TaskInput, TaskInstance
+from models.task import TaskInput, Task, TaskKind
 
 
-@dataclass
 class PublisherClient:
-    """A client for publishing tasks to the manager.
+    """A client for publishing and retrieving tasks."""
 
-    ### Attributes
-    - `manager_config`: The configuration for the manager.
-    - `_manager_client`: The manager client.
+    # Broker
+    _broker_config: BrokerConfig
+    _broker_client: BrokerClient
 
-    ### Methods
-    - `publish_task`: Publish a task to the manager.
-    - `get_task`: Get the status of a task by its UUID.
-    """
-
-    manager_config: ManagerConfig
+    # Manager
+    _manager_config: ManagerConfig
     _manager_client: ManagerClient
 
-    def __init__(self, manager_config: ManagerConfig):
+    def __init__(self, manager_config: ManagerConfig, broker_config: BrokerConfig):
         self._manager_config = manager_config
-        self._manager_client = ManagerClient(manager_config)
+        self._manager_client = ManagerClient(config=manager_config)
 
-    async def publish_task(self, task_kind: str, input_data: TaskInput) -> TaskInstance:
+        self._broker_config = broker_config
+        # TODO - Init broker client
+
+    async def publish_task(
+        self,
+        task_kind: str | TaskKind,
+        input_data: Optional[TaskInput] = None,
+        task_id: Optional[UUID] = None,
+        priority: int = 0,
+    ) -> Task:
         """Publish a task to the manager.
 
         ### Arguments
-        - `task_kind`: The kind of the task.
+        - `task_kind`: The kind of the task. Can either be in the format of `worker_kind:task_name` string or a `TaskKind` object.
         - `input_data`: The data to publish.
 
         ### Returns
         - `TaskInstance`: The task instance.
         """
 
-        return await self._manager_client.publish_task(task_kind, input_data)
+        # Convert the task kind to a TaskKind if it is a string
+        kind: TaskKind = (
+            TaskKind.from_str(task_kind) if isinstance(task_kind, str) else task_kind
+        )
 
-    async def get_task(self, task_id: UUID, long_poll: bool = False) -> TaskInstance:
+        # Create a task with base values
+        task = Task(
+            id=task_id or uuid4(),
+            task_kind=kind,
+            input_data=input_data,
+            priority=priority,
+        )
+
+        # Publish the task to the manager
+        # TODO: Implement task publishing to RabbitMQ
+
+        return task
+
+    async def get_task(
+        self, task_id: UUID, override_retry_options: Optional[RetryOptionsBase] = None
+    ) -> Task:
         """Get the status of a task by its UUID.
 
         ### Arguments
         - `task_id`: The UUID of the task.
-        - `long_poll`: Whether to long poll for the task to finish.
+        - `override_retry_options`: The retry options to use if you want to override the default ones.
 
         ### Returns
-        - `TaskInstance`: The task instance.
+        - `Task`: The task.
         """
 
-        task = await self._manager_client.get_task(task_id)
-
-        if long_poll:
-            while not task.has_finished:
-                await asyncio.sleep(1)
-                task = await self._manager_client.get_task(task_id)
-
-        return task
+        return await self._manager_client.get_task(
+            task_id, override_retry_options=override_retry_options
+        )
