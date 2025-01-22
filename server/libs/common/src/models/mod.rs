964975965abb862afc27d7c08468637f5ec43fca
Tiburso <manuelmariamtc@gmail.com>
2025-01-22 20:10:25 +0100

    refactor: remove db_common library and update Worker model to include task kinds

--- server/libs/common/src/models/mod.rs
+++ server/libs/common/src/models/mod.rs
@@ -1,11 +1,9 @@
-pub mod task_kinds;
-pub mod tasks;
-pub mod time_format;
-pub mod worker_kinds;
-pub mod workers;
+mod task_kinds;
+mod tasks;
+mod time_format;
+mod workers;
 
 pub use task_kinds::*;
 pub use tasks::*;
 pub use time_format::*;
-pub use worker_kinds::*;
 pub use workers::*;
