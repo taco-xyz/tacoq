8ee0b265f2f667e18907d416ce11b3e66295e1f3
Pedro Ribeiro <47680931+tubarao312@users.noreply.github.com>
2025-01-23 17:41:50 +0000

    feat: Add worker kind registration to manager client

--- client_sdks/python/src/manager/client.py
+++ client_sdks/python/src/manager/client.py
@@ -7,10 +7,13 @@ from aiohttp import ClientSession, ClientConnectorError
 from aiohttp_retry import RetryClient, RetryOptionsBase
 
 from manager.config import ManagerConfig
-from models.task import Task
+from models.task import Task, WorkerKindBrokerInfo
 
 TASK_PATH = "/tasks"
 """ Base path for task CRUD operations."""
+
+WORKER_KIND_PATH = "/worker-kind"
+""" Base path for worker kind registration."""
 
 HEALTH_PATH = "/health"
 """ Base path for health checking."""
@@ -33,11 +36,22 @@ class ManagerStates(str, Enum):
 
 
 class ManagerClient(BaseModel):
-    """Abstracts the manager API task retrieval."""
+    """Abstracts the manager API."""
 
     config: ManagerConfig
+    """Configuration for the manager client."""
 
-    # Check whether the manager is healthy
+    worker_kind_broker_info: dict[str, WorkerKindBrokerInfo]
+    """Cache of worker kind broker information. Read 
+    `get_worker_kind_broker_info()` for more details."""
+
+    def __init__(self, config: ManagerConfig):
+        self.config = config
+        self.worker_kind_broker_info = {}
+
+    # ================================
+    # Health Checking
+    # ================================
 
     async def check_health(
         self, override_retry_options: Optional[RetryOptionsBase] = None
@@ -46,7 +60,7 @@ class ManagerClient(BaseModel):
         tests are run to notify the user if the manager is not healthy or even
         running at all.
 
-        ### Parameters
+        ### Args
         - `override_retry_options`: Retry options to override the default ones
 
         ### Returns
@@ -70,14 +84,16 @@ class ManagerClient(BaseModel):
         except ClientConnectorError:
             return ManagerStates.NOT_REACHABLE
 
+    # ================================
     # Task Get/Set Operations
+    # ================================
 
     async def get_task(
         self, task_id: UUID, override_retry_options: Optional[RetryOptionsBase] = None
     ) -> Task:
         """Get a task by its UUID.
 
-        ### Parameters
+        ### Args
         - `task_id`: UUID of the task to retrieve
         - `override_retry_options`: Retry options to override the default ones
 
@@ -97,3 +113,72 @@ class ManagerClient(BaseModel):
                 resp.raise_for_status()
                 data = await resp.json()
                 return Task(**data)
+
+    # ================================
+    # Worker Information
+    # ================================
+
+    async def get_worker_kind_broker_info(
+        self, worker_kind: str
+    ) -> WorkerKindBrokerInfo:
+        """Get the broker information for a worker kind. Checks locally before
+        fetching from the manager.
+
+        ### Usage
+        The publisher and the worker use this information
+        in different ways:
+        - The publisher uses the routing key to correctly route the task to the
+        appropriate queues. This information is fetched from the manager the first
+        time a new worker kind is used in task submission.
+        - The worker uses the queue name to consume the correct queue. This information
+        is fetched from the manager on startup, and the worker will only connect to the
+        queue once it gets a response from the manager.
+
+
+        ### Constraints
+        This information could be computed within the SDK, but for the sake of consistency
+        and easier portability to each language, we let the manager handle this logic instead.
+        The manager also handles queue and exchange creation, so the SDKs can be lightweight.
+
+        This means the entire system hinges on the manager being required for any of the
+        SDKs to start up, which is something we want to address in the future. One idea
+        for addressing this is having a Rust-based library that each SDK can use to set
+        up the necessary queues and exchanges, and the logic is shared across all languages.
+
+        ### Args:
+        - `worker_kind`: The worker kind to register
+
+        ### Returns
+        - `WorkerKindBrokerInfo`: Information about the worker kind.
+        """
+
+        if worker_kind not in self.worker_kind_broker_info:
+            self.worker_kind_broker_info[
+                worker_kind
+            ] = await self._register_worker_kind(worker_kind)
+
+        return self.worker_kind_broker_info[worker_kind]
+
+    async def _register_worker_kind(self, worker_kind: str) -> WorkerKindBrokerInfo:
+        """Registers a worker kind and fetches the information needed to
+        interact with the broker.
+
+        ### Args
+        - `worker_kind`: The worker kind to register
+
+        ### Returns
+        - `WorkerKindBrokerInfo`: Information about the worker kind.
+        """
+
+        async with ClientSession() as session:
+            retry_client = RetryClient(
+                session,
+                retry_options=self.config.retry_options,
+            )
+
+            async with retry_client.put(
+                f"{self.config.base_url}{WORKER_KIND_PATH}/{worker_kind}"
+            ) as resp:
+                resp.raise_for_status()
+                data = await resp.json()
+                return WorkerKindBrokerInfo(**data)
