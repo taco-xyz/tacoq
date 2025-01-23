0c7ca7e3391226900e6b1d7a3e37bc3dd41e05d9
Pedro Ribeiro <47680931+tubarao312@users.noreply.github.com>
2025-01-23 17:49:38 +0000

    refactor: Remove irrelevant fields from broker config

--- client_sdks/python/src/broker/config.py
+++ client_sdks/python/src/broker/config.py
@@ -6,9 +6,3 @@ class BrokerConfig(BaseModel):
 
     url: str
     """ The URL of the broker. """
-
-    durable: bool = True
-    """ Whether exchanges and queues are saved on disk."""
-
-    auto_delete: bool = False
-    """ Whether exchanges and queues are deleted when they are no longer in use."""
