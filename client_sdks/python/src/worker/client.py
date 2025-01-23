bca4bfbb6407f54b1ee38101aa1ea228e5d908ae
Pedro Ribeiro <47680931+tubarao312@users.noreply.github.com>
2025-01-23 18:27:31 +0000

    rework: Refactor worker client to use new broker clients

--- client_sdks/python/src/worker/client.py
+++ client_sdks/python/src/worker/client.py
@@ -2,12 +2,33 @@ from dataclasses import dataclass
 from typing import Callable, Awaitable, Optional, Dict
 
 import asyncio
-from uuid import UUID
-from broker import create_broker_instance, BrokerClient
+from broker import WorkerBrokerClient
 from manager import ManagerClient
-from models.task import TaskInput, TaskOutput
+from models.task import Task, TaskInput, TaskOutput
 
 from worker.config import WorkerApplicationConfig
+
+
+# =========================================
+# Errors
+# =========================================
+
+
+class TaskNotRegistered(Exception):
+    """Exception raised when a task is not registered."""
+
+    def __init__(
+        self,
+        task_kind: str,
+        registered_tasks: Dict[str, Callable[[TaskInput], Awaitable[TaskOutput]]],
+    ):
+        self.message = f"Task {task_kind} not registered for this worker. Available tasks: {registered_tasks.keys()}"
+        super().__init__(self.message)
+
+
+# =========================================
+# Worker Application
+# =========================================
 
 
 @dataclass
@@ -15,10 +36,19 @@ class WorkerApplication:
     """A worker application that processes tasks from a task queue."""
 
     _config: WorkerApplicationConfig
+    """ The configuration for this worker application. """
+
     _registered_tasks: Dict[str, Callable[[TaskInput], Awaitable[TaskOutput]]]
-    _broker_client: Optional[BrokerClient]
+    """ All the tasks that this worker application can handle. """
+
+    _broker_client: Optional[WorkerBrokerClient]
+    """ The broker client that this worker application uses. """
+
+    _queue_name: Optional[str]
+    """ The queue name that this worker application listens to. """
+
     _manager_client: ManagerClient
-    _queue_name: str
+    """ The manager client that this worker application uses to interface with the manager service. """
 
     def __init__(self, config: WorkerApplicationConfig):
         # Init all the clients
@@ -27,11 +57,9 @@ class WorkerApplication:
         self._manager_client = ManagerClient(config.manager_config)
         self._registered_tasks = {}
 
-        # Get the broker information for this worker kind
-        worker_kind_info = asyncio.run(
-            self._manager_client.get_worker_kind_broker_info(self._config.kind)
-        )
-        self._queue_name = worker_kind_info.queue_name
+    # ================================
+    # Task Registration & Execution
+    # ================================
 
     def register_task(
         self, kind: str, task: Callable[[TaskInput], Awaitable[TaskOutput]]
@@ -67,48 +95,7 @@ class WorkerApplication:
 
         return decorator
 
-    async def _register_worker(self):
-        """Register this worker with the manager and initialize broker connection.
-
-        ### Raises
-        - `ConnectionError`: If connection to manager or broker fails
-        """
-        worker = await self._manager_client.register_worker(
-            self._config.name, list(self._registered_tasks.keys())
-        )
-        self._id = worker
-
-        # For this ideally we would get the broker information from the manager
-        self._broker_client = create_broker_instance(
-            self._config.broker_config, self._config.name, str(self._id)
-        )
-        await self._broker_client.connect()
-
-    async def _unregister_worker(self):
-        """Unregister from the manager and clean up broker connection.
-
-        ### Raises
-        - `ValueError`: If worker is not registered
-        """
-        if self._id is None:
-            raise ValueError("Worker is not registered.")
-
-        try:
-            if self._broker_client:
-                await self._broker_client.disconnect()
-        except Exception as e:
-            raise ValueError(f"Error during broker disconnect: {e}")
-
-        try:
-            await self._manager_client.unregister_worker(self._id)
-        except Exception as e:
-            raise ValueError(f"Error during worker unregister: {e}")
-
-        # Clear local variables state
-        # Important for hot reloading code
-        self.cleanup()
-
-    async def _execute_task(self, kind: str, input_data: TaskInput, task_id: UUID):
+    async def _execute_task(self, task: Task):
         """Execute a task and update its status in the manager.
 
         ### Parameters
@@ -119,21 +106,55 @@ class WorkerApplication:
         ### Raises
         - `ValueError`: If task kind is not registered
         """
-        task_func = self._registered_tasks.get(kind)
+
+        # Find task handler
+        task_func = self._registered_tasks.get(task.task_kind.name)
         if task_func is None:
-            raise ValueError(f"Task {kind} not registered.")
+            raise TaskNotRegistered(task.task_kind.name, self._registered_tasks)
+
+        # Compute task result
+        result: Optional[TaskOutput | Exception] = None
+        is_error: bool = False
 
         try:
-            # Check what to do with the task result
-            result = await task_func(input_data)
-            await self._manager_client.update_task_result(
-                task_id, result, is_error=False
-            )
+            result = await task_func(task.input_data)
         except Exception as e:
-            # Log the exception (could improve error handling)
-            await self._manager_client.update_task_result(
-                task_id, {"error": str(e)}, is_error=True
-            )
+            result = Exception(e)
+            is_error = True
+
+        # TODO Submit task result via broker
+        print(f"Task {task.task_kind.name} {is_error}: {result}")
+        ...
+
+    # ================================
+    # Worker Lifecycle
+    # ================================
+
+    async def entrypoint(self):
+        """Start the worker application.
+
+        This method registers the worker kind with the manager,
+        starts listening for tasks, and handles graceful shutdown.
+        """
+
+        # Register the worker kind with the manager
+        worker_kind_info = await self._manager_client.get_worker_kind_broker_info(
+            self._config.kind
+        )
+        self._queue_name = worker_kind_info.queue_name
+
+        # Init the broker client using the queue name of the worker kind
+        self._broker_client = WorkerBrokerClient(
+            self._config.broker_config, self._queue_name
+        )
+
+        # Begin loop
+        try:
+            await self._listen()
+        except asyncio.CancelledError:
+            pass
+        finally:
+            await self._cleanup()
 
     async def _listen(self):
         """Listen for tasks of a specific kind from the broker.
@@ -141,43 +162,20 @@ class WorkerApplication:
         ### Raises
         - `RuntimeError`: If broker client is not initialized
         """
-        if not self._broker_client:
-            raise RuntimeError("Broker client is not initialized.")
 
-        input_data: TaskInput
-        task_id: UUID
-        task_kind: str
+        if self._broker_client is None:
+            raise RuntimeError("Broker client not initialized")
 
         try:
-            async for (
-                input_data,
-                task_id,
-                task_kind,
-            ) in self._broker_client.listen():
-                await self._execute_task(task_kind, input_data, task_id)
+            async for task in self._broker_client.listen():
+                await self._execute_task(task)
         except asyncio.CancelledError:
             await self._broker_client.disconnect()
 
-    async def entrypoint(self):
-        """Start the worker application.
-
-        This method registers the worker, starts listening for tasks,
-        and handles graceful shutdown.
-        """
-        await self._register_worker()
-
-        try:
-            await self._listen()
-        except asyncio.CancelledError:
-            pass
-        finally:
-            await self._unregister_worker()
-
-    def cleanup(self):
+    async def _cleanup(self):
         """Cleanup the worker application.
 
         This method is called when the worker is shutting down. Used for cleaning internal state.
         """
-        self._broker_client = None
-        self._registered_tasks = {}
-        self._id = None
+        # TODO - Add tracing and potential cleanup logic if needed
+        ...
