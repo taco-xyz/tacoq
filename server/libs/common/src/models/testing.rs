9889beb09c24741cc41c2e592ebcaf1c748a16af
Tiburso <manuelmariamtc@gmail.com>
2025-01-25 12:16:33 +0100

    refactor: rename function names in testing libs

--- server/libs/common/src/models/testing.rs
+++ server/libs/common/src/models/testing.rs
@@ -2,7 +2,7 @@
 pub mod test {
     use crate::models::{Worker, WorkerKind};
 
-    pub fn setup_worker_kinds() -> Vec<WorkerKind> {
+    pub fn get_worker_kinds() -> Vec<WorkerKind> {
         vec![
             WorkerKind::new("kind1", "task1", "task1"),
             WorkerKind::new("kind2", "task2", "task2"),
@@ -10,7 +10,7 @@ pub mod test {
         ]
     }
 
-    pub fn setup_workers(worker_kinds: &Vec<WorkerKind>) -> Vec<Worker> {
+    pub fn get_workers(worker_kinds: &Vec<WorkerKind>) -> Vec<Worker> {
         vec![
             Worker::new("worker1", &worker_kinds[0].name),
             Worker::new("worker2", &worker_kinds[1].name),
