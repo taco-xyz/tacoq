4da7b127169c2238d0bd596ae76ad2d8ba08d756
Pedro Ribeiro <47680931+tubarao312@users.noreply.github.com>
2025-01-28 01:14:37 +0000

    fix: Improve pydantic model usage for WorkerApplication

--- client_sdks/python/src/worker/client.py
+++ client_sdks/python/src/worker/client.py
@@ -1,4 +1,3 @@
-from dataclasses import dataclass
 from typing import Callable, Awaitable, Optional, Dict
 
 import asyncio
@@ -6,6 +5,7 @@ from broker import WorkerBrokerClient
 from manager import ManagerClient
 from models.task import Task, TaskInput, TaskOutput
 
+from pydantic import BaseModel
 from worker.config import WorkerApplicationConfig
 
 
@@ -14,7 +14,7 @@ from worker.config import WorkerApplicationConfig
 # =========================================
 
 
-class TaskNotRegistered(Exception):
+class TaskNotRegisteredError(Exception):
     """Exception raised when a task is not registered."""
 
     def __init__(
@@ -31,31 +31,27 @@ class TaskNotRegistered(Exception):
 # =========================================
 
 
-@dataclass
-class WorkerApplication:
+class WorkerApplication(BaseModel):
     """A worker application that processes tasks from a task queue."""
 
-    _config: WorkerApplicationConfig
+    config: WorkerApplicationConfig
     """ The configuration for this worker application. """
 
-    _registered_tasks: Dict[str, Callable[[TaskInput], Awaitable[TaskOutput]]]
-    """ All the tasks that this worker application can handle. """
-
-    _broker_client: Optional[WorkerBrokerClient]
-    """ The broker client that this worker application uses. """
-
-    _queue_name: Optional[str]
-    """ The queue name that this worker application listens to. """
-
-    _manager_client: ManagerClient
+    _manager_client: Optional[ManagerClient] = None
     """ The manager client that this worker application uses to interface with the manager service. """
 
-    def __init__(self, config: WorkerApplicationConfig):
-        # Init all the clients
-        self._config = config
-        self._id = None
-        self._manager_client = ManagerClient(config.manager_config)
+    _registered_tasks: Dict[str, Callable[[TaskInput], Awaitable[TaskOutput]]] = {}
+    """ All the tasks that this worker application can handle. """
+
+    _broker_client: Optional[WorkerBrokerClient] = None
+    """ The broker client that this worker application uses. """
+
+    _queue_name: Optional[str] = None
+    """ The queue name that this worker application listens to. """
+
+    def model_post_init(self, _) -> None:
         self._registered_tasks = {}
+        self._manager_client = ManagerClient(config=self.config.manager_config)
 
     # ================================
     # Task Registration & Execution
@@ -108,9 +104,9 @@ class WorkerApplication:
         """
 
         # Find task handler
-        task_func = self._registered_tasks.get(task.task_kind.name)
+        task_func = self._registered_tasks.get(task.task_kind)
         if task_func is None:
-            raise TaskNotRegistered(task.task_kind.name, self._registered_tasks)
+            raise TaskNotRegisteredError(task.task_kind, self._registered_tasks)
 
         # Compute task result
         result: Optional[TaskOutput | Exception] = None
@@ -123,12 +119,34 @@ class WorkerApplication:
             is_error = True
 
         # TODO Submit task result via broker
-        print(f"Task {task.task_kind.name} {is_error}: {result}")
+        print(f"Task {task.task_kind} {is_error}: {result}")
         ...
 
     # ================================
     # Worker Lifecycle
     # ================================
+
+    async def _init_broker_client(self):
+        """Initialize the broker client for this worker.
+
+        ### Raises
+        - `RuntimeError`: If manager client is not initialized
+        """
+        if self._manager_client is None:
+            raise RuntimeError("Manager client not initialized")
+
+        # Register the worker kind with the manager
+        worker_kind_info = await self._manager_client.get_worker_kind_broker_info(
+            self.config.kind
+        )
+        self._queue_name = worker_kind_info.queue_name
+
+        # Init the broker client using the queue name of the worker kind
+        self._broker_client = WorkerBrokerClient(
+            config=self.config.broker_config,
+            task_assignment_queue_name=self._queue_name,
+        )
+        await self._broker_client.connect()
 
     async def entrypoint(self):
         """Start the worker application.
@@ -136,17 +154,7 @@ class WorkerApplication:
         This method registers the worker kind with the manager,
         starts listening for tasks, and handles graceful shutdown.
         """
-
-        # Register the worker kind with the manager
-        worker_kind_info = await self._manager_client.get_worker_kind_broker_info(
-            self._config.kind
-        )
-        self._queue_name = worker_kind_info.queue_name
-
-        # Init the broker client using the queue name of the worker kind
-        self._broker_client = WorkerBrokerClient(
-            self._config.broker_config, self._queue_name
-        )
+        await self._init_broker_client()
 
         # Begin loop
         try:
