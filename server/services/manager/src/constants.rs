a46e5af06f44e860ab03390e64fdde4913357fd7
Tiburso <manuelmariamtc@gmail.com>
2025-02-04 16:53:00 +0100

    change queue name in constant

--- server/services/manager/src/constants.rs
+++ server/services/manager/src/constants.rs
@@ -1,5 +1,4 @@
 // This is the file for all the project constants
-pub const TASK_INPUT_QUEUE: &str = "task_assignment_queue";
-pub const TASK_RESULT_QUEUE: &str = "task_results";
+pub const TASK_INPUT_QUEUE: &str = "manager_queue";
 
 pub const TASK_OUTPUT_EXCHANGE: &str = "task_assignment_exchange";
