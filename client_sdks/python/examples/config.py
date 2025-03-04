8971d80bcf1f65bc8b83b661f2e85c46c4c63e3c
Pedro Ribeiro <47680931+tubarao312@users.noreply.github.com>
2025-03-04 01:02:19 +0000

    docs: Update example

--- client_sdks/python/examples/config.py
+++ client_sdks/python/examples/config.py
@@ -1,16 +1,16 @@
-from broker import BrokerConfig
-from manager.config import ManagerConfig
-from worker import WorkerApplicationConfig
+from core.infra.relay.config import RelayConfig
+from core.infra.broker.config import BrokerConfig
+from worker.client import WorkerApplicationConfig
 
 # Manager and Broker configurations
-manager_config = ManagerConfig(url="http://localhost:3000")
+relay_config = RelayConfig(url="http://localhost:3000")
 broker_config = BrokerConfig(url="amqp://user:password@localhost:5672")
 
 # Worker configuration
 worker_config = WorkerApplicationConfig(
     name="test_worker",
     kind="test_worker",
-    manager_config=manager_config,
+    relay_config=relay_config,
     broker_config=broker_config,
     broker_prefetch_count=5,
 )
