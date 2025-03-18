8ea8654388b27f5d3e2b660c284bb7f874748e66
Pedro Ribeiro <47680931+tubarao312@users.noreply.github.com>
2025-03-18 03:30:30 +0000

    feat: Add logo

--- docs/src/components/react/Logo.tsx
+++ docs/src/components/react/Logo.tsx
@@ -1,5 +1,6 @@
 // Next Imports
 import Link from "next/link";
+import Image from "next/image";
 
 /**
  * Logo component that displays the application logo and title
@@ -7,27 +8,23 @@ import Link from "next/link";
  */
 export default function Logo() {
   return (
-    <Link
-      href="/"
-      className="flex flex-row items-center space-x-0.5 cursor-pointer custom-tab-outline-offset-6 rounded-sm transition-all ease-in-out duration-150"
-    >
-      <div className="sm:size-[27px] size-[24px] sm:shadow-xl shadow-lg rounded-sm dark:shadow-white/30 shadow-zinc-400/20 dark:bg-white bg-zinc-400 transition-all ease-in-out duration-150">
-        <svg
-          width="100"
-          height="100"
-          viewBox="0 0 100 100"
-          className="w-full h-full"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            d="M35.6695 87L42.1195 34.6C42.3529 32.8 42.9195 31.3667 43.8195 30.3C44.7195 29.2333 46.0529 28.5833 47.8195 28.35H24.5195L26.2695 14.3H78.8695L77.1195 28.35H58.8195L51.6195 87H35.6695Z"
-            className="dark:fill-zinc-900 fill-white transition-[fill] ease-in-out duration-150"
-          />
-        </svg>
+    <Link href="/">
+      <div>
+        <Image
+          src="/TacoDivisionLogoDark.svg"
+          alt="Logo"
+          width={40}
+          height={40}
+          className="block dark:hidden"
+        />
+        <Image
+          src="/TacoDivisionLogoLight.svg"
+          alt="Logo"
+          width={40}
+          height={40}
+          className="hidden dark:block"
+        />
       </div>
-      <p className="dark:text-white text-zinc-800 font-mono font-bold tracking-tight sm:text-3xl text-2xl transition-all ease-in-out duration-150 leading-none ">
-        .doc
-      </p>
     </Link>
   );
 }
