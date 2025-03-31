1ebf1292eddcbc58a7606b1c2b4f187e2a27b675
Pedro Ribeiro <47680931+tubarao312@users.noreply.github.com>
2025-03-31 01:32:49 +0100

    feat: Add encoding types

---
+++ client_sdks/python/tacoq/core/encoding/__init__.py
@@ -0,0 +1,15 @@
+from tacoq.core.encoding.encoder import (
+    pydantic_encoder,
+    pydantic_decoder,
+    EncoderFunction,
+    DecoderFunction,
+    Data,
+)
+
+__all__ = [
+    "pydantic_encoder",
+    "pydantic_decoder",
+    "EncoderFunction",
+    "DecoderFunction",
+    "Data",
+]
